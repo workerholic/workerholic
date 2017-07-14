2aae56d1e024186d5cba070e3a7080373d042bd6
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-07-14 18:20:58 -0400

    seperate serialization to job serializer

--- lib/job.rb
+++ lib/job.rb
@@ -1,15 +1,12 @@
-require 'yaml'
 require_relative 'queue'
+require_relative 'job_serializer'
 
 module Workerholic
   module Job
-    def perform_async(*args)
-      Queue.new.enqueue(serialized(self.class, args))
-    end
-
-    def serialized(klass, args)
-      job_obj = [klass, args]
-      ::YAML.dump(job_obj)
+    def perform_async(queue_name = 'default', *args)
+      job = [self.class, args]
+      serialized_job = JobSerializer.serialize(job)
+      Queue.new(queue_name).enqueue(serialized_job)
     end
   end
 end
