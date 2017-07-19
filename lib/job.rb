52025d268b0595bc502b4452d9648bf1943f19ac
tim-lee92 <tim-lee92@hotmail.com>
2017-07-18 17:59:22 -0700

    Add statistics attribute to jobs

--- lib/job.rb
+++ lib/job.rb
@@ -6,7 +6,20 @@ module Workerholic
     def perform_async(*args)
       raise ArgumentError if self.method(:perform).arity != args.size
 
-      job = { class: self.class, arguments: args }
+      job = { class: self.class,
+              arguments: args,
+              statistics: {
+                execute_on: Time.now,
+                enqueued_on: Time.now,
+                finished: nil,
+                retries: 0,
+                errors: [],
+                success: false,
+                time_start: nil,
+                time_completed: nil,
+                completed: nil
+              }
+            }
       serialized_job = JobSerializer.serialize(job)
 
       Queue.new(queue_name).enqueue(serialized_job)
