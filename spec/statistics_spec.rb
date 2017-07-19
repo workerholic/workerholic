3d66a8e658742b63a8d566782b5b0cabda097a7d
tim-lee92 <tim-lee92@hotmail.com>
2017-07-19 16:41:21 -0700

    Add spec for to_hash method

--- spec/statistics_spec.rb
+++ spec/statistics_spec.rb
@@ -9,4 +9,19 @@ describe Workerholic::Statistics do
     expect(statistics.started_at).to be_nil
     expect(statistics.completed_at).to be_nil
   end
+
+  it 'initializes attributes with arguments' do
+    enqueuing_time = Time.now.to_f - 86400
+    started_at_time = Time.now.to_f
+    completed_at_time = Time.now.to_f + 86400
+    options = {
+      enqueued_at: enqueuing_time,
+      retry_count: 4,
+      errors: ['Your job is bad and you should feel bad'],
+      started_at: started_at_time,
+      completed_at: completed_at_time
+    }
+
+    expect(Workerholic::Statistics.new(options).to_hash).to eq(options)  
+  end
 end
