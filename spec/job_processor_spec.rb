3fe9ff47f6d37332aac432ce7c696f1db0fc93b2
tim-lee92 <tim-lee92@hotmail.com>
2017-07-19 14:04:38 -0700

    Change specs to accomodate stat tracking

--- spec/job_processor_spec.rb
+++ spec/job_processor_spec.rb
@@ -1,7 +1,8 @@
-require_relative './spec_helper.rb'
+require_relative './spec_helper'
 
-require_relative '../lib/job_processor.rb'
-require_relative './helpers/job_tests.rb'
+require_relative '../lib/job_processor'
+require_relative './helpers/job_tests'
+require_relative '../lib/statistics'
 
 class SimpleJobTestWithError
   include Workerholic::Job
@@ -17,7 +18,12 @@ end
 
 describe Workerholic::JobProcessor do
   it 'processes a simple job' do
-    serialized_job = Workerholic::JobSerializer.serialize({ class: SimpleJobTest, arguments: ['test job'] })
+    serialized_job = Workerholic::JobSerializer.serialize({
+                       class: SimpleJobTest,
+                       arguments: ['test job'],
+                       statistics: Workerholic::Statistics.new.to_hash
+                     })
+
     job_processor = Workerholic::JobProcessor.new(serialized_job)
     simple_job_result = SimpleJobTest.new.perform('test job')
 
@@ -25,7 +31,11 @@ describe Workerholic::JobProcessor do
   end
 
   it 'processes a complex job' do
-    serialized_job = Workerholic::JobSerializer.serialize({ class: ComplexJobTest, arguments: ['test job', { a: 1, b: 2 }, [1, 2, 3]] })
+    serialized_job = Workerholic::JobSerializer.serialize({
+                       class: ComplexJobTest,
+                       arguments: ['test job', { a: 1, b: 2 }, [1, 2, 3]],
+                       statistics: Workerholic::Statistics.new.to_hash
+                     })
     job_processor = Workerholic::JobProcessor.new(serialized_job)
     complex_job_result = ComplexJobTest.new.perform('test job', { a: 1, b: 2 }, [1, 2, 3])
 
@@ -33,7 +43,11 @@ describe Workerholic::JobProcessor do
   end
 
   it 'raises a custom error when processing a job with error' do
-    serialized_job = Workerholic::JobSerializer.serialize({ class: SimpleJobTestWithError, arguments: [] })
+    serialized_job = Workerholic::JobSerializer.serialize({
+                       class: SimpleJobTestWithError,
+                       arguments: [],
+                       statistics: Workerholic::Statistics.new.to_hash
+                     })
     job_processor = Workerholic::JobProcessor.new(serialized_job)
 
     expect { job_processor.process }.to raise_error(Workerholic::JobProcessingError)
