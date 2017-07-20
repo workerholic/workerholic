67b4a3e67b1e035e585175634f7b3371276f8aa8
tim-lee92 <tim-lee92@hotmail.com>
2017-07-20 15:36:18 -0700

    Removed duplicate files

--- lib/job_processor.rb
+++ lib/job_processor.rb
@@ -18,14 +18,15 @@ module Workerholic
       job_stats = Workerholic::Statistics.new(job[:statistics])
 
       begin
-        job_stats[:started_at] = Time.now
+        job_stats[:started_at] = Time.now.to_f
         finished_job = job_class.new.perform(*job_args)
-        completed_time = Time.now
+        completed_time = Time.now.to_f
         job_stats[:completed_at] = completed_time
         LogManager.new('info').log("Your job from class #{job_class} was completed on #{completed_time}.")
         finished_job
       rescue Exception => e
         job_stats.errors.push([e.class, e.message])
+        LogManager.new('error').log("Your job from class #{job_class} was unsuccessful. Retrying in 30 seconds.")
         JobRetry.new(job: job)
       end
 
