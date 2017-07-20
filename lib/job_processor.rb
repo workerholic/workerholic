d4ae25529c1497b651dd2a238822afabc865d50c
tim-lee92 <tim-lee92@hotmail.com>
2017-07-20 16:12:42 -0700

    Add attr_reader, and formatting

--- lib/job_processor.rb
+++ lib/job_processor.rb
@@ -1,6 +1,7 @@
 require_relative 'job_serializer'
 require_relative 'statistics'
 require_relative 'job_retry'
+require_relative 'log_manager'
 
 module Workerholic
   class JobProcessingError < StandardError; end
@@ -19,15 +20,18 @@ module Workerholic
 
       begin
         job_stats[:started_at] = Time.now.to_f
-        finished_job = job_class.new.perform(*job_args)
+        job_result = job_class.new.perform(*job_args)
         completed_time = Time.now.to_f
         job_stats[:completed_at] = completed_time
+
         LogManager.new('info').log("Your job from class #{job_class} was completed on #{completed_time}.")
-        finished_job
+
+        job_result
       rescue Exception => e
         job_stats.errors.push([e.class, e.message])
-        LogManager.new('error').log("Your job from class #{job_class} was unsuccessful. Retrying in 30 seconds.")
         JobRetry.new(job: job)
+
+        LogManager.new('error').log("Your job from class #{job_class} was unsuccessful. Retrying in 10 seconds.")
       end
 
       # Push job into some collection
--- lib/log_manager.rb
+++ lib/log_manager.rb
@@ -2,17 +2,19 @@ require 'logger'
 
 module Workerholic
   class LogManager
+    attr_reader :logger, :severity
+
     def initialize(severity)
       @logger = Logger.new(STDOUT)
       @severity = severity
     end
 
     def log(message)
-      @logger.formatter = proc do |severity, datetime, progname, msg|
+      logger.formatter = proc do |severity, datetime, progname, msg|
         "#{severity}: #{msg}\n"
       end
 
-      @logger.send(@severity, message)
+      logger.send(severity, message)
     end
   end
 end
