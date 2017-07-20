505f76e6483ebd7bf1617994386bc18081693d84
Konstantin M <sub@linuxpath.com>
2017-07-20 14:29:53 -0700

    Refactor JobScheduler parameters format Add job options Initial scheduled job functionality

--- lib/job_scheduler.rb
+++ lib/job_scheduler.rb
@@ -6,35 +6,41 @@ module Workerholic
     attr_reader :sorted_set, :set_name, :queue
     attr_accessor :alive
 
-    def initialize(set_name = 'workerholic:scheduled_jobs', queue_name = 'workerholic:main')
-      @sorted_set = SortedSet.new(set_name)
+    def initialize(opts={})
+      @sorted_set = SortedSet.new(opts[:set_name] || 'workerholic:scheduled_jobs')
       @set_name = set_name
-      @queue = Queue.new(queue_name)
+      @queue = Queue.new(opts[:queue_name] || 'workerholic:main')
       @alive = true
     end
 
     def start
       @scheduler_thread = Thread.new do
         while alive
-          poll_scheduled
+          enqueue_due_jobs
         end
       end
     end
 
     def job_due?
-      job_execution_time = sorted_set.peek.last
-      Time.now.to_f >= job_execution_time
+      job = sorted_set.peek
+
+      if job
+        job_execution_time = job.last
+        Time.now.to_f >= job_execution_time
+      end
     end
 
     def schedule(serialized_job, score)
       sorted_set.add(serialized_job, score)
     end
 
-    def poll_scheduled
+    def enqueue_due_jobs
       if job_due?
-        serialized_job, job_execution_time = sorted_set.peek
-        sorted_set.remove(job_execution_time)
-        queue.enqueue(serialized_job)
+        while job_due?
+          serialized_job, job_execution_time = sorted_set.peek
+          sorted_set.remove(job_execution_time)
+          queue.enqueue(serialized_job)
+        end
       else
         sleep(5)
       end
--- spec/helpers/job_tests.rb
+++ spec/helpers/job_tests.rb
@@ -2,25 +2,20 @@ require_relative '../../lib/job.rb'
 
 class SimpleJobTest
   include Workerholic::Job
+  job_options queue_name: 'workerholic:test:queue'
 
   def perform(s)
     s
-  end
-
-  def queue_name
-    'workerholic:test:queue'
   end
 end
 
 class ComplexJobTest
   include Workerholic::Job
+  job_options queue_name: 'workerholic:test:queue'
+
 
   def perform(arg1, arg2, arg3)
     [arg1, arg2, arg3]
-  end
-
-  def queue_name
-    'workerholic:test:queue'
   end
 end
 
