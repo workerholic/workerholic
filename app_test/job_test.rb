cc69762a360a7e384df22e0cb129513a90a349f4
tim-lee92 <tim-lee92@hotmail.com>
2017-07-24 13:31:57 -0700

    Add multiple types of jobs for testing/benchmarking

--- app_test/job_test.rb
+++ app_test/job_test.rb
@@ -12,7 +12,60 @@ class JobTestSlow
   include Workerholic::Job
 
   def perform(str, num)
-    sleep(1)
+    sleep(0.5)
     puts "#{num} - #{str}"
+  end
+end
+
+class ThreadKiller
+  include Workerholic::Job
+
+  def perform(string, n)
+    puts "#{n}. #{string}"
+    Thread.main.kill
+  end
+end
+
+class LargeArg
+  include Workerholic::Job
+
+  def perform(arr, n)
+    puts n
+  end
+end
+
+class ManyArgs
+  include Workerholic::Job
+
+  def perform(n, *args)
+    puts "#{n}: #{args}"
+  end
+end
+
+# A CPU-blocking operation
+class HeavyCalculation
+  include Workerholic::Job
+
+  def perform(n, arr)
+    arr = bubble_sort(arr)
+    puts "#{n}: #{arr[0..9]}"
+  end
+
+  def bubble_sort(array)
+    return array if array.size <= 1
+
+    unsorted = true
+
+    while unsorted do
+      unsorted = false
+      0.upto(array.size-2) do |i|
+        if array[i] > array[i+1]
+          array[i], array[i+1] = array[i+1], array[i]
+          unsorted = true
+        end
+      end
+    end
+
+    array
   end
 end
