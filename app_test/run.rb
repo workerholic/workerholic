2388b94091c9e3040670e52748a023da08ef0657
tim-lee92 <tim-lee92@hotmail.com>
2017-07-24 13:32:39 -0700

    Add enqueueing for the new benchmarking jobs

--- app_test/run.rb
+++ app_test/run.rb
@@ -1,6 +1,31 @@
 require_relative 'job_test'
+require_relative '../lib/manager.rb'
 
-20.times do |n|
-  JobTestFast.new.perform_async('NON BLOCKING', n)
-  JobTestSlow.new.perform_async('BLOCKING', n)
+# 100000.times do |n|
+#   JobTestFast.new.perform_async('NONBLOCKING', n)
+#   # sleep(0.0015)
+# end
+
+# 10000.times do |n|
+#   JobTestSlow.new.perform_async('BLOCKING', n)
+#   sleep(0.0015)
+# end
+
+# 100000.times do |n|
+#   ThreadKiller.new.perform_async('Kill', n)
+# end
+
+# arg = Array.new(10000, 'string')
+# 10000.times do |n|
+#   LargeArg.new.perform_async(arg, n)
+# end
+
+# unsorted_array = (1..10000).to_a.shuffle
+unsorted_array = (1..1000).to_a.shuffle
+1000.times do |n|
+  HeavyCalculation.new.perform_async(n, unsorted_array)
 end
+
+# 100000.times do |n|
+#   ManyArgs.new.perform_async(n, [1, 2, 3], { key: 'value'}, :symb, 'string', 22, false)
+# end
