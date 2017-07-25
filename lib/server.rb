8d8688655633beb173412228a8f3f422bf0dc4e8
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-07-25 18:12:53 -0400

    move workers count and connection pool with dynamic size creation to Workerholic module

--- lib/server.rb
+++ lib/server.rb
@@ -3,4 +3,11 @@ $LOAD_PATH << __dir__
 require 'workerholic'
 
 auto_balance = ARGV.any? { |arg| arg == '--auto-balance' }
+workers_count = ARGV.find { |arg| arg.match? /^--workers=\d+$/ }
+
+if workers_count
+  workers_count = workers_count[/\d+/].to_i
+  Workerholic.workers_count = workers_count
+end
+
 Workerholic::Manager.new(auto_balance: auto_balance).start
