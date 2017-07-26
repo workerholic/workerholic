52f30e727fc6a461fee0a4e2eb5f480a51a94d93
Konstantin M <k@minevskiy.com>
2017-07-26 16:51:57 -0700

    Update queue.rb

--- lib/workerholic/queue.rb
+++ lib/workerholic/queue.rb
@@ -1,7 +1,7 @@
 module Workerholic
   # Handles background job enqueueing/dequeuing functionality
   class Queue
-    attr_reader :storage, :name
+    attr_reader :storage, :name, :logger
 
     def initialize(name = 'workerholic:queue:main')
       @storage = Storage::RedisWrapper.new
