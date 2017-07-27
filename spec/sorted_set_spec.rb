638d9dd2b19a9a42b5b8f917870ab5bbe4856af6
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-07-27 18:46:04 -0400

    add tests for new sorted set methods

--- spec/sorted_set_spec.rb
+++ spec/sorted_set_spec.rb
@@ -5,21 +5,34 @@ describe Workerholic::SortedSet do
   let(:redis) { Redis.new }
   let(:sorted_set) { Workerholic::SortedSet.new(TEST_SCHEDULED_SORTED_SET) }
 
-  after { redis.del(TEST_SCHEDULED_SORTED_SET) }
-
   it 'adds a serialized job to the sorted set' do
     serialized_job = Workerholic::JobSerializer.serialize(job)
     score = Time.now.to_f
-    expect(sorted_set.add(score, serialized_job)).to eq(true)
+
+    expect(sorted_set.storage).to receive(:add_to_set).and_return(true)
+
+    sorted_set.add(serialized_job, score)
   end
 
   it 'removes due job from the sorted set' do
     serialized_job = Workerholic::JobSerializer.serialize(job)
     score = Time.now.to_f
+    redis.zadd(TEST_SCHEDULED_SORTED_SET, score, serialized_job)
 
-    sorted_set.add(score, serialized_job)
     sorted_set.remove(score)
 
     expect(redis.zcount(TEST_SCHEDULED_SORTED_SET, 0, '+inf')).to eq(0)
+  end
+
+  it 'returns first element from sorted set' do
+    expect(sorted_set.storage).to receive(:peek)
+
+    sorted_set.peek
+  end
+
+  it 'checks if set is empty' do
+    expect(sorted_set.storage).to receive(:sorted_set_size)
+
+    sorted_set.empty?
   end
 end
