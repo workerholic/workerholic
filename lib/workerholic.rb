de1a5fdde2f8d68f3a6f870093ec09c9f2eaa634
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-07-27 21:26:07 -0400

    attempt at requiring rails

--- lib/workerholic.rb
+++ lib/workerholic.rb
@@ -26,12 +26,8 @@ require 'workerholic/log_manager'
 require_relative '../app_test/job_test' # require the application code
 
 module Workerholic
-  # DEFAULTS = {
-  #   workers_count: 25
-  # }
-
   def self.workers_count
-    @workers_count || 25 # DEFAULTS[:workers_count]
+    @workers_count || 25
   end
 
   def self.workers_count=(num)
