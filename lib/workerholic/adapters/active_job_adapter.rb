7c28d7ba5763cf93e8e4fd08e8254c042720137e
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-07-29 13:46:50 -0400

    update active job adapter

--- lib/workerholic/adapters/active_job_adapter.rb
+++ lib/workerholic/adapters/active_job_adapter.rb
@@ -3,11 +3,15 @@ module ActiveJob
     class WorkerholicAdapter
       def enqueue(job)
         job_data = job.serialize
+        queue_name = "workerholic:queue:#{job_data['queue_name']}"
 
-        JobWrapper.new.perform_async(
-          class: job_data[:job_class],
-          arguments: job_data[:arguments]
-        )
+        job = JobWrapper.new
+        job.instance_variable_set(:@queue_name, queue_name)
+
+        # capture job class so it can be passed to `Base.execute` in `JobWrapper#perform`
+        job.instance_variable_set(:@class, job_data['job_class'])
+
+        job.perform_async(*job_data['arguments'])
       end
 
       class JobWrapper
@@ -19,6 +23,4 @@ module ActiveJob
       end
     end
   end
-
-  autoload :WorkerholicAdapter
 end
