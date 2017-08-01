4cad3549e251c72ec9c2c3539b19a17eccfc4772
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-08-01 18:49:14 -0400

    extract start up logic to a starter class

--- lib/workerholic/cli.rb
+++ lib/workerholic/cli.rb
@@ -5,8 +5,6 @@ require 'optparse'
 require 'singleton'
 
 module Workerholic
-  PROCESSES_IDS = [Process.pid]
-
   class CLI
     include Singleton
 
@@ -18,12 +16,9 @@ module Workerholic
 
     def run
       parse_options
-      set_options
 
-      load_app
-
-      # Manager.new(auto_balance: options[:auto_balance]).start
-      start
+      Starter.options = options
+      Starter.start
     end
 
     private
@@ -69,52 +64,6 @@ module Workerholic
           end
         end
       end.parse!
-    end
-
-    def set_options
-      Workerholic.workers_count = options[:workers] if options[:workers]
-    end
-
-    def load_app
-      if File.exist?('./config/environment.rb')
-        require File.expand_path('./config/environment.rb')
-
-        require 'workerholic/adapters/active_job_adapter'
-
-        ActiveSupport.run_load_hooks(:before_eager_load, Rails.application)
-        Rails.application.config.eager_load_namespaces.each(&:eager_load!)
-      elsif options[:require]
-        file_path = File.expand_path(options[:require])
-
-        if File.exist?(file_path)
-          require file_path
-        else
-          logger.info('The file you specified to load your application is not valid!')
-
-          exit
-        end
-      else
-        logger.info('If you are using a Rails app, make sure to navigate to your root directory before starting Workerholic!')
-        logger.info('If you are not using a Rails app, you can load your app by using the option --require and specifying the file needing to be required in order to load your application.')
-
-        exit
-      end
-    end
-
-    def start
-      if options[:processes] && options[:processes] > 1
-        options[:processes].times do
-          PROCESSES_IDS << fork do
-            Manager.new(auto_balance: options[:auto_balance]).start
-          end
-        end
-
-        sleep
-      else
-        Manager.new(auto_balance: options[:auto_balance]).start
-      end
-    rescue SystemExit, Interrupt
-      exit
     end
   end
 end
