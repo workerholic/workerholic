c2511fd78991bf3bfe3325f7a6b98040e00e0fb2
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-08-01 23:38:24 -0400

    fix job retry  spec to match new implementation

--- spec/job_retry_spec.rb
+++ spec/job_retry_spec.rb
@@ -15,7 +15,7 @@ describe Workerholic::JobRetry do
     Workerholic::JobRetry.new(
       job: job,
       sorted_set: Workerholic::SortedSet.new(TEST_SCHEDULED_SORTED_SET)
-    )
+    ).retry
 
     expect(job.retry_count).to eq(1)
   end
@@ -26,7 +26,7 @@ describe Workerholic::JobRetry do
     Workerholic::JobRetry.new(
       job: job,
       sorted_set: Workerholic::SortedSet.new(TEST_SCHEDULED_SORTED_SET)
-    )
+    ).retry
 
     expect((job.execute_at - Time.now.to_f).ceil).to eq(30)
   end
@@ -37,7 +37,7 @@ describe Workerholic::JobRetry do
     Workerholic::JobRetry.new(
       job: job,
       sorted_set: Workerholic::SortedSet.new(TEST_SCHEDULED_SORTED_SET)
-    )
+    ).retry
 
     serialized_job = Workerholic::JobSerializer.serialize(job)
 
@@ -50,7 +50,7 @@ describe Workerholic::JobRetry do
     Workerholic::JobRetry.new(
       job: job,
       sorted_set: Workerholic::SortedSet.new(TEST_SCHEDULED_SORTED_SET)
-    )
+    ).retry
 
     expect(redis.exists(TEST_SCHEDULED_SORTED_SET)).to eq(false)
   end
