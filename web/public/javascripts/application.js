2a99204a53f36ae141b0443c82a13d3588d6983e
tim-lee92 <tim-lee92@hotmail.com>
2017-08-02 09:38:35 -0700

    Tracks memory. Refactored some more.

--- web/public/javascripts/application.js
+++ web/public/javascripts/application.js
@@ -2,14 +2,29 @@ var App = {
   queuedJobsCountHistory: [],
   failedJobsCountHistory: [],
   jobsCompletedHistory: [],
+  jobsCompletedPerSecondHistory: [],
   totalMemoryHistory: [],
+  maxTime: 180,
+  freshDataCount: function() {
+    return (this.maxTime / 5) + 1
+  },
   tab: null,
+  removeStaleData: function() {
+    if (this.queuedJobsCountHistory.length > this.freshDataCount) {
+      this.queuedJobsCountHistory.pop();
+      this.failedJobsCountHistory.pop();
+      this.jobsCompletedHistory.pop();
+      this.totalMemoryHistory.pop();
+    }
+  },
   getOverviewData: function() {
     $.ajax({
       url: '/overview-data',
       context: this,
       success: function(data) {
         var deserializedData = JSON.parse(data);
+        var workersCount = deserializedData.workers_count;
+        var scheduledJobsCount = deserializedData.scheduled_jobs;
 
         var completedJobs = deserializedData.completed_jobs.reduce(function(sum, subArray) {
           return sum + subArray[1];
@@ -25,29 +40,34 @@ var App = {
           return sum + queue[1];
         }, 0);
 
-        // var totalMemory = deserializedData.memory_usage ...
+        var memoryUsage = deserializedData.memory_usage;
+        var totalMemoryUsage = 0;
+
+        for (id in memoryUsage) {
+          totalMemoryUsage = totalMemoryUsage + parseInt(memoryUsage[id]);
+          // if ($('#process_' + id).length === 1) {
+          $('#process_' + id).text(parseInt(memoryUsage[id]) / 1000 + ' MB');
+          // } else {
+          //   $('.nested').last().after("<tr class='nested'><td>" + id + "</td><td id='process_'" + id + ">" + memoryUsage[id] + "</td></tr>")
+          // }
+        }
 
         this.queuedJobsCountHistory.unshift(queuedJobsCount);
         this.failedJobsCountHistory.unshift(failedJobsCount);
         this.jobsCompletedHistory.unshift(completedJobs);
-        // totalMemoryHistory.unshift(totalMemory); Waiting for data
+        this.totalMemoryHistory.unshift(totalMemoryUsage / 1000);
+        this.jobsCompletedPerSecondHistory.unshift((this.jobsCompletedHistory[0] - this.jobsCompletedHistory[1]) / 5 || 0);
 
-        if (this.queuedJobsCountHistory.length > 13) {
-          this.queuedJobsCountHistory.pop();
-          this.failedJobsCountHistory.pop();
-          this.jobsCompletedHistory.pop();
-          this.totalMemoryHistory.pop();
-        }
-
+        this.removeStaleData();
         this.drawChart();
-
-        var workersCount = deserializedData.workers_count;
 
         $('.completed_jobs').text(completedJobs);
         $('.failed_jobs').text(failedJobsCount);
         $('.queue_count').text(queuedJobs.length);
         $('.queued_jobs_count').text(queuedJobsCount);
+        $('.scheduled_jobs').text(scheduledJobsCount);
         $('.workers_count').text(workersCount);
+        $('.memory_usage').text(totalMemoryUsage / 1000 + ' MB');
       }
     });
   },
@@ -107,7 +127,7 @@ var App = {
         tickColor: 'silver',
         animationEnabled: true,
         title: 'Time ago (s)',
-        maximum: 60
+        maximum: this.maxTime
       },
       toolTip: {
         shared: true
@@ -125,21 +145,7 @@ var App = {
         color: "blue",
         markerType: 'circle',
         lineThickness: 2,
-        dataPoints: [
-          { x: '0', y: (this.jobsCompletedHistory[0] - this.jobsCompletedHistory[1]) / 5 || 0 },
-          { x: '5', y: (this.jobsCompletedHistory[1] - this.jobsCompletedHistory[2]) / 5 || 0 },
-          { x: '10', y: (this.jobsCompletedHistory[2] - this.jobsCompletedHistory[3]) / 5 || 0 },
-          { x: '15', y: (this.jobsCompletedHistory[3] - this.jobsCompletedHistory[4]) / 5 || 0 },
-          { x: '20', y: (this.jobsCompletedHistory[4] - this.jobsCompletedHistory[5]) / 5 || 0 },
-          { x: '25', y: (this.jobsCompletedHistory[5] - this.jobsCompletedHistory[6]) / 5 || 0 },
-          { x: '30', y: (this.jobsCompletedHistory[6] - this.jobsCompletedHistory[7]) / 5 || 0 },
-          { x: '35', y: (this.jobsCompletedHistory[7] - this.jobsCompletedHistory[8]) / 5 || 0 },
-          { x: '40', y: (this.jobsCompletedHistory[8] - this.jobsCompletedHistory[9]) / 5 || 0 },
-          { x: '45', y: (this.jobsCompletedHistory[9] - this.jobsCompletedHistory[10]) / 5 || 0 },
-          { x: '50', y: (this.jobsCompletedHistory[10] - this.jobsCompletedHistory[11]) / 5 || 0 },
-          { x: '55', y: (this.jobsCompletedHistory[11] - this.jobsCompletedHistory[12]) / 5 || 0 },
-          { x: '60', y: (this.jobsCompletedHistory[12] - this.jobsCompletedHistory[13]) / 5 || 0 },
-        ]
+        dataPoints: this.setDataPoints(this.jobsCompletedPerSecondHistory, this.freshDataCount()),
       }]
     });
 
@@ -156,7 +162,7 @@ var App = {
         animationEnabled: true,
         title: 'Time ago (s)',
         // minimum: 0,
-        maximum: 60
+        maximum: this.maxTime,
       },
       toolTip: {
         shared: true
@@ -174,7 +180,7 @@ var App = {
         name: "Queued Jobs",
         markerType: "circle",
         color: "#F08080",
-        dataPoints: this.setDataPoints(this.queuedJobsCountHistory),
+        dataPoints: this.setDataPoints(this.queuedJobsCountHistory, this.freshDataCount()),
       }],
     });
 
@@ -191,7 +197,7 @@ var App = {
         animationEnabled: true,
         title: 'Time ago (s)',
         // minimum: 0,
-        maximum: 60
+        maximum: this.maxTime,
       },
       toolTip: {
         shared: true
@@ -209,7 +215,7 @@ var App = {
           color: "#20B2AA",
           markerType: 'circle',
           lineThickness: 2,
-          dataPoints: this.setDataPoints(this.failedJobsCountHistory),
+          dataPoints: this.setDataPoints(this.failedJobsCountHistory, this.freshDataCount()),
         },
       ]
     });
@@ -227,7 +233,7 @@ var App = {
         animationEnabled: true,
         title: 'Time ago (s)',
         // minimum: 0,
-        maximum: 60
+        maximum: this.maxTime
       },
       toolTip: {
         shared: true
@@ -245,7 +251,7 @@ var App = {
         color: "#20B2AA",
         markerType: 'circle',
         lineThickness: 2,
-        dataPoints: this.setDataPoints(this.totalMemoryHistory),
+        dataPoints: this.setDataPoints(this.totalMemoryHistory, this.freshDataCount()),
       }],
     });
 
@@ -254,22 +260,13 @@ var App = {
     processedJobsChart.render();
     totalMemoryChart.render();
   },
-  setDataPoints: function(array) {
-    data = [
-      { x: '0', y: array[0] },
-      { x: '5', y: array[1] },
-      { x: '10', y: array[2] },
-      { x: '15', y: array[3] },
-      { x: '20', y: array[4] },
-      { x: '25', y: array[5] },
-      { x: '30', y: array[6] },
-      { x: '35', y: array[7] },
-      { x: '40', y: array[8] },
-      { x: '45', y: array[9] },
-      { x: '50', y: array[10] },
-      { x: '55', y: array[11] },
-      { x: '60', y: array[12] },
-    ];
+  setDataPoints: function(array, count) {
+    var data = [];
+
+    for (var i = 0; i <= count; i++) {
+      var point = { x: (i * 5).toString(), y: array[i] };
+      data.push(point);
+    }
 
     return data;
   },
@@ -304,6 +301,7 @@ var App = {
   bindEvents: function() {
     $('#memory_usage').on('click', function(e) {
       $('.nested th').toggle();
+      $('.nested td').toggle();
     });
   },
   init: function() {
