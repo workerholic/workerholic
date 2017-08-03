ba61549504c3abf50f20a59a24d4a7b931d6818c
Konstantin M <sub@linuxpath.com>
2017-08-02 19:56:39 -0700

    Initial historical API

--- lib/workerholic/statistics_api.rb
+++ lib/workerholic/statistics_api.rb
@@ -55,14 +55,43 @@ module Workerholic
       storage.hash_keys(namespace)
     end
 
-    private
+    def self.history_for_period(category, period = 30)
+      namespace = "workerholic:stats:historical:#{category}"
 
-    def self.storage
-      @storage ||= Storage::RedisWrapper.new
+      start_time = self.convert_to_time_ago(period)
+      end_time = Time.now.to_i
+      # with scores
+      jobs_range = storage.members_in_range(namespace, start_time, end_time)
+
+      jobs_range.map do |range|
+        jobs_count, time_int = range
+        [jobs_count, self.time_to_date(time_int)]
+      end
     end
 
-    def self.logger(message)
-      @log ||= LogManager.new
+    def self.history_for_class(category, klass, period = 30)
+      namespace = "workerholic:stats:historical:#{category}:#{klass}"
+
+      start_time = self.convert_to_time_ago(period)
+      end_time = Time.now.to_i
+      # with scores
+      classes_range = storage.members_in_range(namespace, start_time, end_time)
+
+      classes_range.map do |range|
+        classes_count, time_int = range
+        date = self.convert_time_to_date(time_int)
+        [classes_count, date]
+      end
+    end
+
+    private
+
+    def self.convert_to_time_ago(days)
+      Time.now.to_i - 86400 * 30 - Time.now.to_i % 86400
+    end
+
+    def self.convert_time_to_date(time_int)
+      Time.at time_int
     end
 
     def self.parse_scheduled_jobs(jobs)
@@ -100,6 +129,14 @@ module Workerholic
       obj[:klass] = obj[:klass].to_s
       obj[:wrapper] = obj[:wrapper].to_s
       obj
+    end
+
+    def self.storage
+      @storage ||= Storage::RedisWrapper.new
+    end
+
+    def self.logger(message)
+      @log ||= LogManager.new
     end
   end
 end
