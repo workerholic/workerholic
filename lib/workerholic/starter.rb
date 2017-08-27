ae62566e09032d82170aaa4861310332d2814b90
Antoine Leclercq <antoine.leclercq.49@gmail.com>
2017-08-27 12:11:55 -0400

    reduce edge case by loading specified file first, if any

--- lib/workerholic/starter.rb
+++ lib/workerholic/starter.rb
@@ -32,10 +32,10 @@ module Workerholic
     end
 
     def self.load_app
-      if File.exist?('./config/environment.rb')
-        load_rails
-      elsif options[:require]
+      if options[:require]
         load_specified_file
+      elsif File.exist?('./config/environment.rb')
+        load_rails
       else
         display_app_load_info
       end
